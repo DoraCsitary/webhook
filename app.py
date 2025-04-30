b28edc0f1898e83826d30d0a90e4ea725c73afbc
User <csitary.dora@gmail.com>
2025-04-30 22:30:30 +0200

    app.py moderating

--- app.py
+++ app.py
@@ -179,4 +179,4 @@ def prompt_test():
     })
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.gateaway("PORT", 5000), debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
